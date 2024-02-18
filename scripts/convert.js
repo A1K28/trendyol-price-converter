d5481fdfee6f55fcfeae5de2f3311565871157ba
Khorbaladze A <aleksandrekhorbaladze@gmail.com>
2024-02-18 13:48:05 +0400

    upd convert

--- scripts/convert.js
+++ scripts/convert.js
@@ -1,11 +1,17 @@
 log("Thank you for using Trendyol Currency Converter! Enjoy");
 
-const liraToGel = getRate();
+const maxCacheTimeInMillis = 6*3600*1000; // 6 hours
 const regex = new RegExp('\\d+\\.\\d+');
 const regexAlt = new RegExp('\\d+');
+const conversionUrl = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/try/gel.json";
 
 function main() {
     log("Window Loaded");
+
+    cacheRate(getRate());
+    fetchRate()
+    // getCachedObject('rate');
+
     setTimeout(function() {document.documentElement.style.display = '';}, 10000);
     let priceDocument = document.evaluate("//div[contains(@class, 'product-price-container')]//font[contains(text(), ' TL')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
     const priceText = priceDocument.textContent;
@@ -14,7 +20,7 @@ function main() {
     if (priceTy == null) {
         priceTy = regexAlt.exec(priceText);
     }
-    const priceGel = (Number(priceTy) * liraToGel).toFixed(2);
+    const priceGel = (Number(priceTy) * getRate()).toFixed(2);
     priceDocument.textContent = priceText + ' (' + priceGel + ' GEL)';
 }
 
@@ -29,5 +35,36 @@ function log(message) {
 // TODO: make this dynamic.
 // send HTTP GET request to get the current exchange rate between TY & GEL
 function getRate() {
-    return 0.086;
+    return 0.085;
+    // let response = httpGet(conversionUrl);
+    // return response['gel'];
+}
+
+function httpGet(theUrl) {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.responseText);
+}
+
+function cacheRate() {
+    let value = getRate();
+    chrome.storage.local.set({ rate : value, savedAt: Date.now() }).then(() => {
+        log("Saving value: " + value + " in cache");
+        return value;
+    });
+}
+
+function fetchRate() {
+    chrome.storage.local.get(['rate', 'savedAt'], function(items) {
+        log(items.rate)
+        log(items.savedAt)
+        if (items.rate && items.savedAt) {
+            if (items.savedAt > Date.now() - maxCacheTimeInMillis) {
+                log(items.rate);
+                return items.rate; // Serialization is auto, so nested objects are no problem
+            }
+        }
+        return cacheRate();
+    });
 }
