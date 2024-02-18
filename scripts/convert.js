ce86bcc214141130e2dc7fdecd0efed0be596bf0
Khorbaladze A <aleksandrekhorbaladze@gmail.com>
2024-02-18 17:15:12 +0400

    Update convert.js

--- scripts/convert.js
+++ scripts/convert.js
@@ -1,8 +1,9 @@
 log("Thank you for using Trendyol Currency Converter! Enjoy");
 
 const maxCacheTimeInMillis = 6*3600*1000; // 6 hours
-const regex = new RegExp('\\d+\\.\\d+ TL');
-const regexAlt = new RegExp('\\d+ TL');
+const regex = new RegExp('\\d+\\.\\d+,\\d+\\s*TL');
+const regex2 = new RegExp('\\d+\\.\\d+\\s*TL');
+const regexAlt = new RegExp('\\d+\\s*TL');
 const conversionUrl = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/try/gel.json";
 var updateInProgress = false;
 
@@ -74,23 +75,38 @@ function processNodes(rate) {
     updateInProgress = true;
     walkDOM(document.body,function(n){
             if (n.nodeType === 3) {
-                log(n.textContent);
-                if (n.textContent && !n.textContent.includes(" GEL)") && (regex.test(n.textContent) || regexAlt.test(n.textContent))) {
-                    let ext;
-                    if (regex.test(n.textContent)) {
-                        ext = regex.exec(n.textContent);
-                    } else if (regexAlt.test(n.textContent)) {
-                        ext = regexAlt.exec(n.textContent);
-                    }
-                    for (let i = 0; i < ext.length; i++) {
-                        const priceGel = (Number(ext[i].replace(" TL", "")) * rate).toFixed(2);
-                        console.log(ext[i] + " " + ext[i].replace(" TL", "") + " " + priceGel)
-                        n.textContent = n.textContent.replace(ext[i], ext[i] + ' (' + priceGel + ' GEL)');
-                    }
+                if (n.textContent && n.textContent === 'TL' && n.parentNode && n.parentNode.parentNode && n.parentNode.parentNode.parentNode) {
+                    log(n.parentNode.textContent);
+                    replacePrice(n.parentNode.parentNode.parentNode, rate);
+                } else {
+                    replacePrice(n, rate);
                 }
             }
         });
     updateInProgress = false;
+}
+
+function replacePrice(n, rate) {
+    if (n.textContent && !n.textContent.includes(" GEL)")) {
+        let ext;
+        if (regex.test(n.textContent)) {
+            ext = regex.exec(n.textContent);
+        } else if (regex2.test(n.textContent)) {
+            ext = regex2.exec(n.textContent);
+        } else if (regexAlt.test(n.textContent)) {
+            ext = regexAlt.exec(n.textContent);
+        } else {
+            return;
+        }
+        for (let i = 0; i < ext.length; i++) {
+            let v = ext[i].replace("TL", "").trim();
+            if (v.includes(",")) {
+                v = v.replaceAll(".","").replace(",", ".");
+            }
+            const priceGel = (Number(v) * rate).toFixed(2);
+            n.textContent = n.textContent.replace(ext[i], ext[i] + ' (' + priceGel + ' GEL)');
+        }
+    }
 }
 
 var timeout = null;
